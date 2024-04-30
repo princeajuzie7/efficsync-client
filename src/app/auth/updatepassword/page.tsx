5cd06d7ddabf2a873dbd5eff0952c3151cc79f51
princeajuzie7 <princeajuzie1@gmail.com>
2024-04-30 18:16:42 +0100

    feat:new config

--- src/app/auth/updatepassword/page.tsx
+++ src/app/auth/updatepassword/page.tsx
@@ -1,28 +1,23 @@
-"use client"
-import { UpdateComponent } from "@/components/auth";
+"use client";
 import React from "react";
-
 import HashLoader from "react-spinners/HashLoader";
-
-
+import { UpdateComponent } from "@/components/auth";
 export default function Page() {
-   
-     
-         
   return (
-    <React.Suspense
-      fallback={
-        <div>
-          <HashLoader
-            color="#2563EB"
-            size={100}
-            style={{ width: "100px", height: "100px" }}
-          />
-        </div>
-      }
-    >
-      <UpdateComponent />
-      
-    </React.Suspense>
+    <div>
+      <React.Suspense
+        fallback={
+          <div>
+            <HashLoader
+              color="#2563EB"
+              size={100}
+              style={{ width: "100px", height: "100px" }}
+            />
+          </div>
+        }
+      >
+        <UpdateComponent />
+      </React.Suspense>
+    </div>
   );
 }
