186faa7b455af1659af7c7a2ad890a7a3a30b16a
princeajuzie7 <princeajuzie1@gmail.com>
2024-05-05 20:27:06 +0100

    feat:

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,26 +1,22 @@
 "use client";
 import React, { useState } from "react";
-
-import Signedhoc from "@/helper/hoc/signedhoc";
-import { useUserContext } from "@/context";
-export function Layout({ children }: { children: React.ReactNode }) {
-
-
-
+import NotSignedhoc from "@/helper/hoc/notSignedhoc";
+export  function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="z-10">
       <main
-       
+      
       >
        
-
+      
           <div className=" ">{children}</div>
          
+       
       </main>
     </div>
   );
 }
 
-const layout = Signedhoc(Layout);
+const layout = NotSignedhoc(Layout);
 export default layout;
